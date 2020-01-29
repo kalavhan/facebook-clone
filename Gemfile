525f2c824d4e08c536b5dad1fd74541f5f80bce4
fegzycole <fergusoniyara@gmail.com>
2020-01-29 10:07:46 +0100

    Add omni-auth facebook gem

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ ruby '2.6.3'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 # Use postgresql as the database for Active Record
 gem 'devise'
+gem 'omniauth-facebook'
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :omniauthable
 end
