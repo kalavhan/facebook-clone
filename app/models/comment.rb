7bf98329e1517012296a3f25a7c28a0f63368a2b
fegzycole <fergusoniyara@gmail.com>
2020-01-31 23:34:02 +0100

    Add links to comments on root path and profile page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  validates :content, presence: true, length: { maximum: 255 }
+  validates :content, presence: true, length: { maximum: 90 }
 end
