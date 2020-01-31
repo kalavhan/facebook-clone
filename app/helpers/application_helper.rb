b8e55b366d33ffe7c0798671c51315de1b137716
kalavhan <josue.brigido1@gmail.com>
2020-01-31 10:02:05 -0700

    Fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@
 
 module ApplicationHelper
   def gravatar_for(user, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.first_name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.first_name, class: 'gravatar')
   end
 end
