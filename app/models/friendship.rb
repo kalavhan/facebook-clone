cfc855c740fd9693e7524f7f38f7a2888e54c095
fegzycole <fergusoniyara@gmail.com>
2020-02-04 22:57:27 +0100

    Add friendship model spec

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+  validates :status, presence: true
 
   def self.get_request(receiver_id, sender_id)
     Friendship.where('receiver_id = ? and sender_id = ?', receiver_id, sender_id)
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Like' do
+    it { should belong_to(:sender) }
+    it { should belong_to(:receiver) }
+    it { should validate_presence_of(:status) }
+  end
 end
