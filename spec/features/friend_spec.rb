b17e6dbfd55640dc0a03a3c749159a231441664b
fegzycole <fergusoniyara@gmail.com>
2020-02-04 23:12:14 +0100

    Add integration test suites for the friendships

--- spec/features/friend_spec.rb
+++ spec/features/friend_spec.rb
@@ -16,4 +16,46 @@ RSpec.feature 'Posts', type: :feature do
     click_on 'Find Friends'
     expect(page).to have_content('Tony Stark')
   end
+
+  it 'Should be able to add a new friend' do
+    visit root_path
+    fill_in 'session[email]', with: 'user@mail.com'
+    fill_in 'session[password]', with: '1234567'
+    click_button 'Log in'
+    click_on 'Find Friends'
+    click_on 'Add Friend'
+    expect(page).to have_content('Pending')
+  end
+
+  it 'Should be able to accept a request' do
+    visit root_path
+    fill_in 'session[email]', with: 'user@mail.com'
+    fill_in 'session[password]', with: '1234567'
+    click_button 'Log in'
+    click_on 'Find Friends'
+    click_on 'Add Friend'
+    click_on 'Log out'
+    fill_in 'session[email]', with: 'tony@mail.com'
+    fill_in 'session[password]', with: '1234567'
+    click_button 'Log in'
+    click_on 'notification'
+    click_on 'Accept'
+    expect(page).to have_content('No Pending Requests')
+  end
+
+  it 'Should be able to reject a request' do
+    visit root_path
+    fill_in 'session[email]', with: 'user@mail.com'
+    fill_in 'session[password]', with: '1234567'
+    click_button 'Log in'
+    click_on 'Find Friends'
+    click_on 'Add Friend'
+    click_on 'Log out'
+    fill_in 'session[email]', with: 'tony@mail.com'
+    fill_in 'session[password]', with: '1234567'
+    click_button 'Log in'
+    click_on 'notification'
+    click_on 'Reject'
+    expect(page).to have_content('No Pending Requests')
+  end
 end
